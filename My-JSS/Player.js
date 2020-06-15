5aa93f23320ae66b7227f65b5bf914f211e1040c
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-06-16 01:41:57 +0700

    tao class Player bao gom cac thuoc tinh va phuong thuc

---
+++ My-JSS/Player.js
@@ -0,0 +1,67 @@
+class Player {
+    get img() {
+        return this._img;
+    }
+
+    set img(value) {
+        this._img = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        this._age = value;
+    }
+
+    get position() {
+        return this._position;
+    }
+
+    set position(value) {
+        this._position = value;
+    }
+
+    get country() {
+        return this._country;
+    }
+
+    set country(value) {
+        this._country = value;
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    set price(value) {
+        this._price = value;
+    }
+
+    constructor(img, name, age, position, country, price) {
+        this._img = img;
+        this._name = name;
+        this._age = age;
+        this._position = position;
+        this._country = country;
+        this._price = price;
+    }
+
+    edit(img, name, age, position, country, price) {
+        this.img = img;
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.country = country;
+        this.price = price;
+    }
+}
